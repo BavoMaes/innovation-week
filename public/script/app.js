0c76dcc6306983db30c98f44abc9c78821154009
TheMrL14 <lennert.vanden.herrewegen@student.ehb.be>
2020-01-29 16:26:35 +0100

    fix : settings grid

--- public/script/app.js
+++ public/script/app.js
@@ -74,7 +74,7 @@ var s = (sketch) => {
     let Benzine = new CarType(startColumn2, endColumn2, carSize, sets.sketch.colors.Benzine, sketch)
     let Electrisch_Hybride = new CarType(startColumn3, endColumn3, carSize, sets.sketch.colors.Electrisch_Hybride, sketch)
 
-    cars = cars.concat(Diesel.createCarArray(11, 6))
+    cars = cars.concat(Diesel.createCarArray(18, 6))
     cars = cars.concat(Benzine.createCarArray(7, 4))
     cars = cars.concat(Electrisch_Hybride.createCarArray(5, 2))
 
